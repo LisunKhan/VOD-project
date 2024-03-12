3904b1aed0a03f192aae101d566d344b6b83456b
Md.Lisun-Ul-Islam <90611108+LisunKhan@users.noreply.github.com>
2024-03-12 15:20:58 +0600

    add verification and email model update

--- vod_project/vod_project/users/models.py
+++ vod_project/vod_project/users/models.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField, DateField, BooleanField
+from django.db.models import CharField, DateField, EmailField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -22,8 +22,7 @@ class User(AbstractUser):
     birth_date = DateField(null=True, blank=True)
     gender = CharField(_("Gender"), choices=GENDER_CHOICES, max_length=1, blank=True)
     address = CharField(_('User Address'), blank=True, max_length=300, null=True)
-    is_email_verified = BooleanField(default=False)
-
+    email = EmailField(unique=True)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
 
