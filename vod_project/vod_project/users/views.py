59c8a2769fb3ea6400d968182c7c962f275c71ad
LisunKhan <islam.lisun@gmail.com>
2024-03-15 15:26:18 +0600

    adding registration email confirmation

--- vod_project/vod_project/users/views.py
+++ vod_project/vod_project/users/views.py
@@ -13,6 +13,7 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from allauth.account.models import EmailAddress
 from .models import User
 from .serializers import UserProfileSerializer, SendPasswordResetEmailSerializer, UserPasswordResetSerializer
 
@@ -72,19 +73,25 @@ class UserLoginAPIView(APIView):
         username = request.data.get('username')
         password = request.data.get('password')
 
-        user = None
-        if '@' in username:
-            try:
-                user = User.objects.get(email=username)
-            except ObjectDoesNotExist:
-                pass
-
+        user = User.objects.all()
+        # if '@' in username:
+        #     try:
+        #         user = User.objects.get(email=username)
+        #     except ObjectDoesNotExist:
+        #         pass
         if not user:
             user = authenticate(username=username, password=password)
 
         if user:
-            token, _ = Token.objects.get_or_create(user=user)
-            return Response({'token': token.key}, status=status.HTTP_200_OK)
+            for users in user:
+                verified = EmailAddress.objects.filter(user_id=users.id).exists()
+                if verified is True:
+                    user = User.objects.get(username=username)
+                    token, _ = Token.objects.get_or_create(user=user)
+                    return Response({'token': token.key}, status=status.HTTP_200_OK)
+                else:
+                    return Response({'error': 'User account is not activated or email is not verified'},
+                                    status=status.HTTP_401_UNAUTHORIZED)
 
         return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
